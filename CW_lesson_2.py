eecd67229cf16124f447f1b02a73144d5fe70dc6
MrSlime <Slime.kostia@gmail.com>
2025-11-21 11:37:09 +0200

    lesson 2

---
+++ CW_lesson_2.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("images/1.jpg")
+
+resize = cv2.resize(image, (image.shape[1]//2, image.shape[0]//2))
+resize = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+
+image = cv2.Canny(resize, 100, 100)
+
+kernel = np.ones((5,5), np.uint8)
+image = cv2.dilate(image, kernel, iterations=1)
+image = cv2.erode(image, kernel, iterations=1)
+cv2.imwrite("Image.jpg", image)
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
